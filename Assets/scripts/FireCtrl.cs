9afbae761d02d5cde10afdbaac8be41e2583f31e
nomco2 <nomco2@naver.com>
2019-01-17 18:40:53 +0900

    1

---
+++ Assets/scripts/FireCtrl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject[] pos;
+    public GameObject bullet;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire();
+    }
+
+    void Fire()
+    {
+        for(int i =0; i< pos.Length; i++)
+        {
+            Instantiate(bullet, pos[i].transform.position, pos[i].transform.rotation);
+        }
+    }
+}
---
+++ Assets/scripts/PlayerCtrl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 300.0f;
+    // Start is called before the first frame update
+
+    public Transform tr; // 플레이어의 위치 값 
+
+
+    float h;
+    float v;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector2 dir = new Vector2(h, v);
+        rb.velocity = dir * speed * Time.deltaTime; //델타 타임 컴퓨터마다 다른 프레임 을 보정해줌
+
+        float size = Camera.main.orthographicSize;
+        float offset = 0.4f;
+        if(tr.position.y >= size - offset)
+        {
+            tr.position = new Vector3(tr.position.x, size, 0);
+        }
+    }
+}
